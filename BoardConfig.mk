448dd5d27e28f5e00bdaddb7cff69fe66c78a1a0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-02 20:46:48 +0200

    andromeda: libinit: Import initial vendor init extension
    * Adapt to andromeda

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,10 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_andromeda
+TARGET_RECOVERY_DEVICE_MODULES := libinit_andromeda
+
 # Media
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API := true
