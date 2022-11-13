add234e36fa9bcc7151de99ced9f20ac93fef475
windeqk <trojan.windows.11@gmail.com>
2022-11-13 02:17:25 +0100

    [kang] Drop wrappedkey [libfs_mgr]Warning: unknown flag: wrappedkey

--- device.mk
+++ device.mk
@@ -223,6 +223,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+## HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0
+
 # IFAA manager
 #PRODUCT_PACKAGES += \
     org.ifaa.android.manager
